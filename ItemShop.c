817a2e5fa5f951b95b87ec0ddbd2c523a8815982
Mary1525 <mariefrey25@gmail.com>
2020-02-03 21:29:39 +0100

    Update ItemShop.c

--- ItemShop.c
+++ ItemShop.c
@@ -3,30 +3,32 @@
 #include <time.h>
 #include <string.h>
 
-struct Objet { //caracteristiques des objets de l'inventaire
-	char nom;
+struct Inventaire { //caracteristiques des objets de l'inventaire
+	char nomObjet[50];
 	int prix;
 };
 
-typedef struct Objet objet; //renomme la structure
+typedef struct Inventaire inventaire; //renomme la structure
 
-void achat (objet*typeObjet,int myMoney) {
-	myMoney -= (*typeObjet).prix;
-
-}
 
 
 int main() {
 	
-	int myMoney = 100;
-	printf("tu as au total %d $\n", myMoney);
+//INVENTAIRE PERSO
 
-	objet armeCorpsaCorps = { "couteau",5};
-	printf("voici le premier objet que tu achetes: le %c est a %d $\n", armeCorpsaCorps.nom, armeCorpsaCorps.prix);
-
-	achat (armeCorpsaCorps.prix);
-	printf("il te reste %d $\n",myMoney);
-
+ inventaire tinvtP[50] = {{"bouclier",10},{"armeAfeu",10},{"armeCorpsAcorps",10}};
+ 
+ printf("Voici votre inventaire :\n%s au prix de %d $\n", tinvtP[0].nomObjet,tinvtP[0].prix);
+ printf("%s au prix de %d $\n", tinvtP[1].nomObjet,tinvtP[1].prix);
+ printf("%s au prix de %d $\n", tinvtP[2].nomObjet,tinvtP[2].prix);
+ 
+ //INVENTAIRE VENDEUR
+ 
+  inventaire tinvtV[50] = {{"possions",10},{"arcs",10},{"fleches",10}};
+ 
+ printf("\nBienvenu cher client ,veux-tu acheter quelque chose ? nous avons bon nombre d'objets ! :\n%s au prix de %d $\n", tinvtV[0].nomObjet,tinvtV[0].prix);
+ printf("%s au prix de %d $\n",tinvtV[1].nomObjet,tinvtV[1].prix);
+ printf("%s au prix de %d $\n",tinvtV[2].nomObjet,tinvtV[2].prix);
 
 	return 0;
 }

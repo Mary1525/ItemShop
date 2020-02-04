ce33e2a2d577165ff5409b2b780bf8430c1aca47
Mary1525 <mariefrey25@gmail.com>
2020-02-04 21:13:17 +0100

    Update ItemShop.c

--- ItemShop.c
+++ ItemShop.c
@@ -10,25 +10,124 @@ struct Inventaire { //caracteristiques des objets de l'inventaire
 
 typedef struct Inventaire inventaire; //renomme la structure
 
+//fonction de tri inventaire perso
+void triTableauP( inventaire tinvtP[50], int nbObjP){
+	
+	inventaire tampon;
+	
+	
+	for(int i=0;i<nbObjP-1;i++){
+		
+		for(int j=0;j<nbObjP-1;j++){
+			if (strcmp(tinvtP[j].nomObjet,tinvtP[j+1].nomObjet)>0){
+				
+				tampon=tinvtP[j];
+				tinvtP[j]=tinvtP[j+1];
+				tinvtP[j+1]=tampon;
+				
+				
+			}
+		}
+	}
+};
+
+//FONCTION DE TRI INVENTAIRE VENDEUR
+void triTableauV( inventaire tinvtV[3], int nbObjV){
+	
+	inventaire tampon;
+	
+	
+	for(int i=0;i<nbObjV-1;i++){
+		
+		for(int j=0;j<nbObjV-1;j++){
+			if (tinvtV[j].prix<tinvtV[j+1].prix){
+				
+				tampon=tinvtV[j];
+				tinvtV[j]=tinvtV[j+1];
+				tinvtV[j+1]=tampon;
+				
+				
+			}
+		}
+	}
+};
+
 
 
 int main() {
 	
+	int nChoice=0;
+	int nbObjP=3;
+	int nbObjV=3;
+	int nPrix=0;
+	char nomNewObject[100];
+
 //INVENTAIRE PERSO
 
  inventaire tinvtP[50] = {{"bouclier",10},{"armeAfeu",10},{"armeCorpsAcorps",10}};
  
- printf("Voici votre inventaire :\n%s au prix de %d $\n", tinvtP[0].nomObjet,tinvtP[0].prix);
- printf("%s au prix de %d $\n", tinvtP[1].nomObjet,tinvtP[1].prix);
- printf("%s au prix de %d $\n", tinvtP[2].nomObjet,tinvtP[2].prix);
- 
  //INVENTAIRE VENDEUR
  
-  inventaire tinvtV[50] = {{"possions",10},{"arcs",10},{"fleches",10}};
+  inventaire tinvtV[3] = {{"potions",5},{"arcs",20},{"fleches",10}};
+
+triTableauP(tinvtP,nbObjP);
  
- printf("\nBienvenu cher client ,veux-tu acheter quelque chose ? nous avons bon nombre d'objets ! :\n%s au prix de %d $\n", tinvtV[0].nomObjet,tinvtV[0].prix);
- printf("%s au prix de %d $\n",tinvtV[1].nomObjet,tinvtV[1].prix);
- printf("%s au prix de %d $\n",tinvtV[2].nomObjet,tinvtV[2].prix);
+ for (int i=0; i< nbObjP;i++){
+	 printf("%s %d\n", tinvtP[i].nomObjet ,tinvtP[i].prix);
+ }
+ 
+ triTableauV(tinvtV,nbObjV);
+ 
+ for (int j=0; j< nbObjV;j++){
+	 printf("%d)%s %d\n",j, tinvtV[j].nomObjet ,tinvtV[j].prix);
+ }
+  scanf ("%d",&nChoice);
+  
+	
+ tinvtP[3]=tinvtV[nChoice];
+ tinvtV[nChoice].prix=0;
+
+ strcpy(tinvtV[nChoice].nomObjet, "");
+ 
+ 
+ 
+ nbObjP++;
+ nbObjV--;
+ 
+ triTableauP(tinvtP,nbObjP);
+ 
+ for (int i=0; i< nbObjP;i++){
+	 printf("%s %d\n", tinvtP[i].nomObjet ,tinvtP[i].prix);
+ }
+ 
+ triTableauV(tinvtV,nbObjV);
+ 
+ for (int j=0; j< nbObjV;j++){
+	 printf("%d)%s %d\n",j, tinvtV[j].nomObjet ,tinvtV[j].prix);
+ }
+
+scanf("%s", nomNewObject);
+scanf("%d",&nPrix);
+
+strcpy (tinvtV[3].nomObjet,nomNewObject);
+tinvtV[3].prix=nPrix;
+nbObjV++;
+
+ printf("--%s %d\n", tinvtV[3].nomObjet ,tinvtV[3].prix);
+ 
+triTableauP(tinvtP,nbObjP);
+ 
+ for (int i=0; i< nbObjP;i++){
+	 printf("%s %d\n", tinvtP[i].nomObjet ,tinvtP[i].prix);
+ }
+ for (int j=0; j< nbObjV;j++){
+	 printf("&&%d)%s %d\n",j, tinvtV[j].nomObjet ,tinvtV[j].prix);
+ }
+ triTableauV(tinvtV,nbObjV);
+ 
+ for (int j=0; j< nbObjV;j++){
+	 printf("%d)%s %d\n",j, tinvtV[j].nomObjet ,tinvtV[j].prix);
+ }
 
 	return 0;
 }
